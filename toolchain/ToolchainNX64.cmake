568f36db719b08d698f4b2cd4b118b3d8ecaff16
Funnbot <22226942+funnbot@users.noreply.github.com>
2023-06-03 16:00:15 -0700

    Update ToolchainNX64.cmake

--- toolchain/ToolchainNX64.cmake
+++ toolchain/ToolchainNX64.cmake
@@ -17,7 +17,7 @@ set(CMAKE_CXX_FLAGS_RELWITHDEBINFO ${NX64_OPT_FLAGS})
 # Target options
 add_compile_options(-mcpu=cortex-a57+fp+simd+crypto+crc)
 add_compile_options(-mno-implicit-float)
-add_compile_options(-Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++98-compat-extra-semi)
+add_compile_options(-Wno-c++98-compat -Wno-c++98-compat-pedantic)
 # Environment
 add_compile_options(-stdlib=libc++)
 add_compile_options(-fPIC)
@@ -33,5 +33,4 @@ add_definitions(-D MATCHING_HACK_NX_CLANG)
 add_link_options(-stdlib=libc++ -nostdlib)
 add_link_options(-fPIC -Wl,-Bsymbolic-functions -shared)
 add_link_options(-fuse-ld=lld)
-add_link_options("-Wl,whole-archive")
-
+add_link_options("-Wl,--whole-archive")
